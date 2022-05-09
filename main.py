9ad6d614411a8e0dac2cdf5317316a71184843a3
Koki <kgoudy64@gmail.com>
2022-05-09 12:41:48 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+#create variable to give input
+number = int(input("Choose a number: "))
+
+#create if statement to check if number is even or odd
+if number % 2 == 0:
+  print("Even")
+else:
+  print("Odd")
